11acc2144fec07a9f351960f0c308e9be1f3deea
bryanhce <72288288+bryanhce@users.noreply.github.com>
2021-05-13 21:58:20 +0800

    first commit, making the master branch

---
+++ Website_JS/home.js
@@ -0,0 +1,16 @@
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160,
+  gutter: 20
+});
