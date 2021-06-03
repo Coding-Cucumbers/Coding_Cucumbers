df2ddd92425da9bdfb404cce4bb2289296979f27
Silas Tay <silastaysl@gmail.com>
2021-06-03 20:17:23 +0800

    Updated resources format and local path

--- Website_JS/blog_history.js
+++ Website_JS/blog_history.js
@@ -19,9 +19,19 @@ function filterSelection(filter) {
     }
 }
 
+//Sheets ID: 1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc
+//Published Sheets: https://docs.google.com/spreadsheets/d/e/2PACX-1vQjv-aC3kieSYEL0g9dImLpQHdtorK5YQOP8ZsSVUnUdidwENS-Iv_ZH1uivLg_6YAvlCL6jroXH95l/pubhtml
+//Sheets as JSON: https://spreadsheets.google.com/feeds/cells/1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc/1/public/values?alt=json-in-script
+//Sh    eets as RSS/XML: https://spreadsheets.google.com/feeds/cells/1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc/1/public/values
+//JavaScript Callback of JSON: https://spreadsheets.google.com/feeds/cells/1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc/1/public/values?alt=json-in-script&callback=doData
 
+//*** Google Sheets as JSON Callback: https://spreadsheets.google.com/feeds/cells/1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc/1/public/full?alt=json
 
+async function loadData() {
+    const response = await fetch("https://spreadsheets.google.com/feeds/cells/1RYNXbxtIeqSmtzJhNca459t8I9Kg_waQRJl-6jBezRc/1/public/full?alt=json").then(response => {return response});
+    const json = await response.json();
+    return json
+}
 
-
-
+loadData().then(response => {console.log(response.feed.entry)})
 
--- Website_JS/blogpost_jquery.js
+++ Website_JS/blogpost_jquery.js
@@ -1,34 +1,35 @@
 $(function(){
 
   // when user inputs an email address
-$(".submit_email").click(function(){
+$("#submit_email").click(function(){
+  $(".error").hide();
        var hasError = false;
        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-       var emailVal = $("#input_box").val();
+       var emailVal = $("#email_input").val();
        if(emailVal == '') {
-           $(".email_input").after('<span class="error"  style="color: red">Please enter your email address.</span>');
+           $("#email_input").after('<span class="error"  style="color: red">Please enter your email address.</span>');
            hasError = true;
        }
 
        else if(!emailReg.test(emailVal)) {
-           $(".email_input").after('<span class="error" style="color: red">Enter a valid email address.</span>');
+           $("#email_input").after('<span class="error" style="color: red">Enter a valid email address.</span>');
            hasError = true;
        }
 
        else {
-         SubForm();
+         SubForm(); //function to submit to google sheets
          window.alert("Thank you for staying in touch!");
        }
-       //ensures that email is cleared
-       document.getElementById(".email_input").value = '';
+       //ensures that email text space is cleared
+       document.getElementById("email_input").value = '';
        if(hasError == true) { return false; }
 });
 
 //to allow user to submit with enter key
-$('.email_input').keypress(function (e) {
+$('#email_input').keypress(function (e) {
   if (e.which === 13) {
-    $('.submit_email').click();
+    $('#submit_email').click();
     return false;
   }
 });
@@ -38,8 +39,8 @@ function SubForm (){
     $.ajax({
         url:'https://api.apispreadsheets.com/data/12899/',
         type:'post',
-        data:$(".email_box_form").serializeArray()
-        //add comma back behind .serializeArray()
+        data:$("#email_box_form").serializeArray()
+        //add comma back behind .serializeArray(), this a checker
         // success: function(){
         //   alert("Form Data Submitted")
         // },
@@ -48,3 +49,4 @@ function SubForm (){
         // }
     });
 }
+})
