25eacca0010ba30b479e6dda052c31dab0bf0e17
Silas Tay <silastay@ohmyhome.com>
2021-06-17 16:25:01 +0800

    Added BeautifulSoup post

--- Website_JS/blogpost.js
+++ Website_JS/blogpost.js
@@ -48,6 +48,40 @@ $('#input_box').keypress(function (e) {
   }
 });
 
+  // when user inputs an email address
+$(".submit_email").click(function(){
+  $(".error").hide();
+       var hasError = false;
+       var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+       var emailVal = $("#email_input").val();
+       if(emailVal == '') {
+           $("#email_input").after('<span class="error"  style="color: red">Please enter your email address.</span>');
+           hasError = true;
+       }
+
+       else if(!emailReg.test(emailVal)) {
+           $("#email_input").after('<span class="error" style="color: red">Enter a valid email address.</span>');
+           hasError = true;
+       }
+
+       else {
+         SubForm(); //function to submit to google sheets
+         window.alert("Thank you for staying in touch!");
+       }
+       //ensures that email text space is cleared
+       document.getElementById("email_input").value = '';
+       if(hasError == true) { return false; }
+});
+
+//to allow user to submit with enter key
+$('#input_box').keypress(function (e) {
+  if (e.which === 13) {
+    $('#submit_button').click();
+    return false;
+  }
+});
+
 //connecting form submissions to googlesheet
 function SubForm (){
     $.ajax({
